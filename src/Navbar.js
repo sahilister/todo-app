cdbdd98dfedd433762de71745d597598ad056b55
sahilister <sahil.dhiman27091999@gmail.com>
2019-11-20 08:19:01 +0530

    Learn JS var usage in react

--- src/Navbar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function Navbar() {
-    return(
-        <navbar className="top">This is the navbar</navbar>
-    )
-}
-export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import './style.css'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+function App() {
+    const date = new Date()
+    const timeOfDay = date.getHours()
+    let printStat
+    if(timeOfDay > 0 && timeOfDay < 12)
+        printStat = "Good Morning"
+    else if(timeOfDay >= 12 && timeOfDay <= 17)
+        printStat = "Good Afternoon"
+    else 
+        printStat = "Good Night";
+    
+    return(<h1> {printStat} </h1>)
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
