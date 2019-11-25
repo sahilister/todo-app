646c8b418a06a2c4217b3fb0cf8097b6778fa896
sahilister <sahil.dhiman27091999@gmail.com>
2019-11-25 16:02:02 +0530

    Maps

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import Joke from './Joke.js'
+import jokesData from './jokesData.js'
 
 function App() {
+    
+    const jokeComponents = jokesData.map(num => {
+            return( 
+                <Joke question={num.question} answer={num.answer} />
+                )
+            })
     return(
         <div>
-            <Joke 
-                info={{ answer:"Make me one with everything." }}
-            />
-            <Joke 
-                info={{question:"You know why you never see elephants hiding up in trees?", answer:"Because they’re really good at it." }}
-            />
-            <Joke 
-                info={{question:"What is red and smells like blue paint?", answer:"Red paint." }}
-            />
-            <Joke 
-                info={{question:"Where does the General keep his armies?", answer:"In his sleevies!" }}
-            />
+            {jokeComponents}
         </div>
-    );
+    )
 }
 export default App;
--- src/Joke.js
+++ src/Joke.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+
 function Joke(props) {
     return(
         <div>
-            <h2 style={{display: props.info.question ? "block":"none"}}>Question: {props.info.question}</h2>
-            <h2>Answer: {props.info.answer}</h2>
+            <h2 style={{display: props.question ? "block":"none"}}>Question: {props.question}</h2>
+            <h2>Answer: {props.answer}</h2>
             <hr />
         </div>
     );
