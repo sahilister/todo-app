39c2c8b4259bf7b9f2c1d554a950244d8c561173
sahilister <sahil.dhiman27091999@gmail.com>
2019-12-05 08:45:00 +0530

    Start conditional

--- src/App.js
+++ src/App.js
@@ -1,27 +1,62 @@
 import React, {Component} from 'react'
-import todoData from './todoData'
-import TodoItem from './TodoItem'
+// import todoData from './todoData'
+// import TodoItem from './TodoItem'
+import Conditional from './Conditional'
 
 
+
+// class App extends Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             todos: todoData
+//         }
+//         this.handleChange = this.handleChange.bind(this)
+//     }
+//     handleChange(id) {
+//         this.setState(prevState => {
+//             const updatedTodos = prevState.todos.map(todo => {
+//                 if(todo.id === id) {
+//                     todo.completed = !todo.completed
+//                 }
+//                 return todo
+//             })
+//             return {
+//                 todos: updatedTodos
+//             }
+//         })
+//     }
+
+//     render() {
+//         const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+//             )
+//         return(
+//             <div>
+//                 {todoItems}
+//             </div>
+//         )
+//     }
+// }
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            todos: todoData
+            isLoading: true
         }
-        this.handleChange = this.handleChange.bind(this)
     }
-    handleChange(id) {
-        
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 1500)
     }
 
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
-            )
         return(
             <div>
-                {todoItems}
+                <Conditional isLoading={this.state.isLoading} />
             </div>
         )
     }
